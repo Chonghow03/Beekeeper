10b9f8c45d5ab74037808ec43d1b38e3457266ab
veixhen <vxwilson@hotmail.com>
2023-05-16 17:00:16 +0800

    task 3 complete

--- threedeebeetree.py
+++ threedeebeetree.py
@@ -12,8 +12,20 @@ class BeeNode:
     item: I
     subtree_size: int = 1
 
+    def __init__(self, key, item):  # i made this
+        self.nodes = {"+++": None, "++-": None, "+-+": None, "+--": None, "-++": None, "-+-": None, "--+": None, "---": None}
+        self.key = key
+        self.item = item
+
     def get_child_for_key(self, point: Point) -> BeeNode | None:
-        raise NotImplementedError()
+        x_self, x_point = self.key[0], point[0]
+        y_self, y_point = self.key[1], point[1]
+        z_self, z_point = self.key[2], point[2]
+
+        # makes a string such as +++ if the point is greater than the node's key in all dimensions
+        strng = ("+" if x_self < x_point else "-") + ("+" if y_self < y_point else "-") + ("+" if z_self < z_point else "-")
+
+        return self.nodes[strng]
 
 
 class ThreeDeeBeeTree(Generic[I]):
@@ -55,7 +67,15 @@ class ThreeDeeBeeTree(Generic[I]):
         return node.item
 
     def get_tree_node_by_key(self, key: Point) -> BeeNode:
-        raise NotImplementedError()
+        return self.get_tree_node_by_key_aux(self.root, key)
+
+    # below function added by me
+    def get_tree_node_by_key_aux(self, current: BeeNode, key: Point) -> BeeNode:
+        if current is None:
+            raise KeyError('Key not found: {0}'.format(key))
+        if key == current.key:
+            return current
+        return self.get_tree_node_by_key_aux(current.get_child_for_key(key), key)
 
     def __setitem__(self, key: Point, item: I) -> None:
         self.root = self.insert_aux(self.root, key, item)
@@ -64,11 +84,29 @@ class ThreeDeeBeeTree(Generic[I]):
         """
             Attempts to insert an item into the tree, it uses the Key to insert it
         """
-        raise NotImplementedError()
+        if current is None:  # base case: at the leaf
+            current = BeeNode(key, item=item)
+            current.subtree_size -= 1  # init() sets it to 1 already
+            self.length += 1
+        elif key == current.key:
+            print(key, current.key)
+            raise ValueError('Inserting duplicate item')
+        else:
+            x_self, x_point = current.key[0], key[0]
+            y_self, y_point = current.key[1], key[1]
+            z_self, z_point = current.key[2], key[2]
+
+            # makes a string such as +++ if the point is greater than the node's key in all dimensions
+            strng = ("+" if x_self < x_point else "-") + ("+" if y_self < y_point else "-") + ("+" if z_self < z_point else "-")
+            current.nodes[strng] = self.insert_aux(current.nodes[strng], key, item)
+
+        current.subtree_size += 1
+        return current
 
     def is_leaf(self, current: BeeNode) -> bool:
         """ Simple check whether or not the node is a leaf. """
-        raise NotImplementedError()
+        return all(value is None for value in current.nodes.values())
+
 
 if __name__ == "__main__":
     tdbt = ThreeDeeBeeTree()
