b0955ea357076384e68942f3bb231ac4480c55d9
Chonghow29 <cong0027@student.monash.edu>
2023-05-18 11:39:25 +0800

    task 5 done

--- beehive.py
+++ beehive.py
@@ -46,14 +46,12 @@ class BeehiveSelector:
         self.max_beehives = max_beehives
         self.heap = MaxHeap(self.max_beehives)
 
-
     def set_all_beehives(self, hive_list: List[Beehive]):
         for beehive in hive_list:
             self.add_beehive(beehive)
     
     def add_beehive(self, hive: Beehive):
         self.heap.add(hive)
-
     
     def harvest_best_beehive(self):
         best_beehive = self.heap.get_max()
@@ -62,4 +60,39 @@ class BeehiveSelector:
         self.add_beehive(best_beehive)
         return emerald
 
+    # List to store the values of the linear equation for each beehive
+    # Calculate the value of the linear equation for each beehive and store it in the list
+    # Replace with the appropriate values for each beehive
+    # Sort the list of values in descending order
+    # Return the kth largest value
+    # Return None if k is out of range
 
+    def kth_largest(self,k: int, a_1: float, a_2: float, a_3: float):
+        hives = []
+
+        for index in range(1,self.heap.length+1):
+            beehive = self.heap.the_array[index]
+            b_x, b_y, b_z = beehive.x, beehive.y, beehive.z
+            value = a_1 * b_x + a_2 * b_y + a_3 * b_z
+            hives.append(value)
+            
+        hives.sort(reverse=True)
+
+        if 0 < k <= len(hives):
+            return hives[k - 1]
+        else:
+            return None
+
+
+if __name__ == "__main__":
+    s = BeehiveSelector(5)
+    b1, b2, b3, b4, b5 = (
+        Beehive(15, 12, 13, capacity=40, nutrient_factor=5, volume=15),
+        Beehive(25, 22, 23, capacity=15, nutrient_factor=8, volume=40),
+        Beehive(35, 32, 33, capacity=40, nutrient_factor=3, volume=40),
+        Beehive(45, 42, 43, capacity=1, nutrient_factor=85, volume=10),
+        Beehive(55, 52, 53, capacity=400, nutrient_factor=5000, volume=0),
+    )
+    for hive in [b1, b2, b3, b4, b5]:
+        s.add_beehive(hive)
+    print(s.kth_largest(4,1.0,1.0,1.0))
--- threedeebeetree.py
+++ threedeebeetree.py
@@ -18,6 +18,23 @@ class BeeNode:
         self.item = item
 
     def get_child_for_key(self, point: Point) -> BeeNode | None:
+        """
+            Explain:
+            - Checks to see if the 3DBT is empty
+
+            Args:
+            -
+
+            Raises:
+            -
+
+            Returns:
+            -
+
+            Complexity:
+            - Worst case:
+            - Best case:
+       """
         x_self, x_point = self.key[0], point[0]
         y_self, y_point = self.key[1], point[1]
         z_self, z_point = self.key[2], point[2]
@@ -40,18 +57,64 @@ class ThreeDeeBeeTree(Generic[I]):
 
     def is_empty(self) -> bool:
         """
-            Checks to see if the 3DBT is empty
-        """
+            Explain:
+            - Checks to see if the 3DBT is empty
+
+            Args:
+            - None
+
+            Raises:
+            - None
+
+            Returns:
+            - Boolean
+                - true, if the length of the 3DBT is 0.
+                - false, if the length is not empty.
+
+            Complexity:
+            - Worst case: O(1), return and if statement
+            - Best case: O(1), return and if statement
+       """
         return len(self) == 0
 
     def __len__(self) -> int:
-        """ Returns the number of nodes in the tree. """
+        """
+            Explain:
+            - Get the length of the 3DBT
 
+            Args:
+            - None
+
+            Raises:
+            - None
+
+            Returns:
+            - the length of the tree
+
+            Complexity:
+            - Worst case: O(1), return statement
+            - Best case: O(1), return statement
+       """
         return self.length
 
     def __contains__(self, key: Point) -> bool:
         """
-            Checks to see if the key is in the 3DBT
+            Explain:
+            - Checks to see if the key is in the 3DBT.
+
+            Args:
+            - key, the point have to find.
+
+            Raises:
+            - KeyError, if the key is not exist.
+
+            Returns:
+            - Boolean
+                - true, if
+
+            Complexity:
+            - Worst case:
+            - Best case:
         """
         try:
             self.get_tree_node_by_key(key)
@@ -61,16 +124,65 @@ class ThreeDeeBeeTree(Generic[I]):
 
     def __getitem__(self, key: Point) -> I:
         """
-            Attempts to get an item in the tree, it uses the Key to attempt to find it
+            Explain:
+            - Attempts to get an item in the tree, it uses the Key to attempt to find it
+
+            Args:
+            -
+
+            Raises:
+            -
+
+            Returns:
+            -
+
+            Complexity:
+            - Worst case:
+            - Best case:
         """
         node = self.get_tree_node_by_key(key)
         return node.item
 
     def get_tree_node_by_key(self, key: Point) -> BeeNode:
+        """
+            Explain:
+            - Get the BeeNode by using get_tree_node_by_key_aux method to search.
+
+            Args:
+            - key, the point to determine the node in 3DBT
+
+            Raises:
+            - None
+
+            Returns:
+            - A BeeNode which is the target node by given key
+
+            Complexity:
+            - Worst case:
+            - Best case:
+        """
         return self.get_tree_node_by_key_aux(self.root, key)
 
     # below function added by me
     def get_tree_node_by_key_aux(self, current: BeeNode, key: Point) -> BeeNode:
+        """
+            Explain:
+            - Search the node located in the subtree rooted at current
+
+            Args:
+            - current
+            - key
+
+            Raises:
+            - KeyError, if the key is not found.
+
+            Returns:
+            - A BeeNode which is the target node by given key.
+
+            Complexity:
+            - Worst case:
+            - Best case:
+        """
         if current is None:
             raise KeyError('Key not found: {0}'.format(key))
         if key == current.key:
@@ -78,11 +190,42 @@ class ThreeDeeBeeTree(Generic[I]):
         return self.get_tree_node_by_key_aux(current.get_child_for_key(key), key)
 
     def __setitem__(self, key: Point, item: I) -> None:
+        """
+            Explain:
+            -
+
+            Args:
+            -
+
+            Raises:
+            -
+
+            Returns:
+            -
+
+            Complexity:
+            - Worst case:
+            - Best case:
+        """
         self.root = self.insert_aux(self.root, key, item)
 
     def insert_aux(self, current: BeeNode, key: Point, item: I) -> BeeNode:
         """
-            Attempts to insert an item into the tree, it uses the Key to insert it
+            Explain:
+            - Attempts to insert an item into the tree, it uses the Key to insert it
+
+            Args:
+            -
+
+            Raises:
+            -
+
+            Returns:
+            -
+
+            Complexity:
+            - Worst case:
+            - Best case:
         """
         if current is None:  # base case: at the leaf
             current = BeeNode(key, item=item)
@@ -104,7 +247,23 @@ class ThreeDeeBeeTree(Generic[I]):
         return current
 
     def is_leaf(self, current: BeeNode) -> bool:
-        """ Simple check whether or not the node is a leaf. """
+        """
+            Explain:
+            - Simple check whether or not the node is a leaf.
+
+            Args:
+            -
+
+            Raises:
+            -
+
+            Returns:
+            -
+
+            Complexity:
+            - Worst case:
+            - Best case:
+        """
         return all(value is None for value in current.nodes.values())
 
 
